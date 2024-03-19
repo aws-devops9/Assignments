9055eddd00fc9504f5d351c6bad00b016221a818
cloudtechprem <cloudtechprem@gmail.com>
2024-03-19 12:39:22 +0530

    1st commit

---
+++ backup-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Develop a backup script that takes a source directory and a destination directory as input and creates a compressed archive (.tar.gz) of the source directory in the destination folder with a timestamp.
+
+R="\e[31m" # Red Colour
+G="\e[32m" # Green colour
+Y="\e[33m" # Yellow colour
+N="\e[0m" # No colour
+
+echo "Please enter Source and Destination to save the files
+
+echo "Please enter Source Directory
+read SOURCE_DIRECTORY
+
+echo "Please enter Destination Directory
+read DESTINATION_DIRECTORY
+
