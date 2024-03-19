70d114feaa50fa428733170d8a2684c81c7c4ef5
cloudtechprem <cloudtechprem@gmail.com>
2024-03-19 13:35:10 +0530

    8th commit

--- backup-script.sh
+++ backup-script.sh
@@ -42,5 +42,5 @@ FILES_TO_MOVE=$(find $SOURCE_DIRECTORY -type f)
 while IFS= read -r line
 do 
     echo "Moving files: $line"
-    mv $line /$DESTINATION_DIRECTORY/
+    mv $line /$DESTINATION_DIRECTORY/$line.zip
 done <<< $FILES_TO_MOVE
